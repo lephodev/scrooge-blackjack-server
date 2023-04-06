8b863101b66504b2dc95326be8ee23111a4be53b
randhir <randhir@websultanate.com>
2023-04-06 13:54:14 +0530

    changes for sit amount and bet amount and wallet

--- Functions/game.js
+++ Functions/game.js
@@ -354,9 +354,9 @@ export const bet = async (io, socket, data) => {
       return;
     }
 
-    if (parseFloat(betAmount) < 10) {
+    if (parseFloat(betAmount) < 1) {
       socket.emit("actionError", {
-        msg: "Minimum bet amount is 10.",
+        msg: "Minimum bet amount is 1.",
       });
       return;
     }
@@ -668,9 +668,9 @@ export const confirmBet = async (io, socket, data) => {
     );
     console.log("GOT player DATA", player);
     if (player && room) {
-      if (betAmount < 10 || !betAmount) {
+      if (betAmount < 1 || !betAmount) {
         socket.emit("actionError", {
-          msg: "Bet amount should be more then 10",
+          msg: "Bet amount should be equal or more than 1",
         });
         return;
       }
